1c4a19c66bf0f6d05a8b98f1f235d66b2bcd30ee
許家豪 <a0955625660@gmail.com>
2025-11-13 18:58:17 +0800

    fix: cors bug

--- base-rest/src/main/java/com/base/rest/config/SecurityConfig.java
+++ base-rest/src/main/java/com/base/rest/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.base.rest.config;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,7 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
             .headers(headers -> headers
                 .frameOptions(FrameOptionsConfig::sameOrigin) // 允許同源的 frame, h2 才能正常訪問
             )
@@ -45,5 +51,29 @@ public class SecurityConfig {
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 允許所有來源 (解決跨域問題)
+        configuration.setAllowedOrigins(List.of("*"));
+
+        // 允許所有 HTTP 方法
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+
+        // 允許所有請求標頭
+        configuration.setAllowedHeaders(List.of("*"));
+
+        // 必須設置為 false，因為設置了 AllowedOrigins("*")
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 將此配置應用於所有路徑
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
